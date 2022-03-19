88853a453d0e18aa1da440b9d97e1cf6df84acc5
unknown <mabiorgraham48@gmail.com>
2022-03-19 03:44:46 -0600

    welcome page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top text-white">
+    <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top text-white">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
